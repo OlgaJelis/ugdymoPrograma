c779c81e435e341737d4a45cfdbc1b530f4b52d4
Olga <andrijankina@gmail.com>
2022-08-22 19:58:24 +0300

    Nuo kažko nusprendžiau pradėti dienoraštį

---
+++ Dienorastis.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dienorastis {
+
+List <Dienorastis> menesioPasiekimai;
+
+public void pridekPasiekima (int vaikoAmzius, int miegoRezimas, String vaikoJudesiai, String vaikoJudesiai,
+                             String psichika, String socialinisTobulejimas, int svorioVidurkis,
+                             int ugioVidurkis, String lavinamiejiZaidimai
+                             ) {
+public void spausindkInformacija () {
+        System.out.println(
+                String.format(
+                        "Menesio pasiekimai [Amzius: %s, Miegas: %s, Judesiai: %s, Psichika: %s, " +
+                                "Socialinis Tobulejimas: %s, Svoris: %s, Ugis: %s, Zaidimai: %s]",
+                        this.amzius;
+                        this.miegas;
+                        this.judesiai;
+                        this.psichika;
+                        this.socialinisTobulejimas;
+                        this.svoris;
+                        this.ugis;
+                        this.zaidimai;
+                        )
+                )
+
+    }
+
+    }
+
+}
